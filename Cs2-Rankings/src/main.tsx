73c29f80c3f8bfe76553a86015c29ecc3ad77908
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-10-31 15:39:25 +0100

    Update PWA

--- Cs2-Rankings/src/main.tsx
+++ Cs2-Rankings/src/main.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('Service worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service worker registration failed:', error);
+    });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
